516a48744085b6a59aaf004a568e66517d6efde7
Breno Lucas <brenolucasmelo@gmail.com>
2021-11-11 22:05:22 -0300

    arquivos da aula 03

--- js/global.js
+++ js/global.js
@@ -1,6 +1,29 @@
 const cardNovaLista = document.getElementById('cardNovaLista')
 const btnNovaLista = document.getElementById('btnNovaLista')
+const novaLista = document.getElementById('novaLista')
+
+const listas = []
 
 btnNovaLista.addEventListener('click', function(){
     cardNovaLista.style.display = 'block'
+})
+
+novaLista.addEventListener('submit', function(event) {
+    event.preventDefault()
+    const inputTitulo = document.querySelector('input[name="tituloLista"]')
+    if(inputTitulo.value !== '') {
+       const newLista = {
+           id: listas.length + 1,
+           titulo: inputTitulo.value
+       }
+       inputTitulo.value = ''
+       listas.push(newLista)
+       window.localStorage.listas = JSON.stringify(listas)
+    }
+    console.log(listas)
+})
+
+novaLista.addEventListener('reset', function(event) {
+    cardNovaLista.style.display = 'none'
+
 })
